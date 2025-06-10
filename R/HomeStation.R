e107037c11136eff024e21de6718eb17b851fc4a
OBrien <William.OBrien@va.gov>
2025-06-10 08:54:26 -0500

    using dplyr pipes (before switch to sqlRender)

--- R/HomeStation.R
+++ R/HomeStation.R
@@ -1,76 +1,113 @@
-
+# todo: person table (non-longitudinal) care site as tie-breaker
 
 HomeStation <- R6::R6Class("HomeStation",
                        list(connection = NULL,
                             cdmSchema = NULL,
                             cohortTableName = NULL,
+                            visitMethod = NULL,
                             homeStation = NULL,
+                            preDays = NULL,
+                            cohort = NULL,
+                            visitOccurrence = NULL,
+                            careSite = NULL,
+
                             initialize = function(connection,
                                                   cdmSchema,
-                                                  cohortTableName){
-                              stopifnot(class(con) == "DatabaseConnectorJdbcConnection")
+                                                  cohortTableName,
+                                                  visitMethod = "mostRecent",
+                                                  preDays = 365,
+                                                  primaryCriteriaType = ){
+                              stopifnot(class(connection) == "DatabaseConnectorJdbcConnection")
+                              stopifnot(visitMethod %in% c("mostRecent", "majority"))
+                              stopifnot(primaryCriteriaType %in% c("condition", "drug", "person"))
 
                               self$connection <- connection
                               self$cdmSchema <- cdmSchema
                               self$cohortTableName <- cohortTableName
-                            }))
+                              self$visitMethod <- visitMethod
+                              self$preDays <- preDays
+
+                            })
+)
+
+
+HomeStation$set("private", "rankingFunction",
+                function(){
+                  if (self$visitMethod == "mostRecent") {
+                    return(\(x) dplyr::arrange(x,
+                                               cohort_definition_id,
+                                               subject_id,
+                                               cohort_start_date,
+                                               desc(visit_start_date)) |>
+                      dplyr::select(-visit_occurrence_id,
+                             -visit_start_date,
+                             -care_site_id))
+
+                  } else if (self$visitMethod == "majority") {
+                    return(\(x) dplyr::count(x,
+                                             cohort_definition_id,
+                                             subject_id,
+                                             cohort_start_date,
+                                             cohort_end_date,
+                                             station) |>
+                             dplyr::arrange(cohort_definition_id,
+                                            subject_id,
+                                            cohort_start_date,
+                                            cohort_end_date,
+                                            desc(n)) |>
+                             dplyr::select(-n))
+                  }
+                }
+)
+
 
 HomeStation$set("public", "getHomeStation",
                 function(){
 
-                  cohort <- dplyr::tbl(self$connection,
-                                       I(self$cohortTableName))
+                  self$cohort <- dplyr::tbl(self$connection,
+                                            I(self$cohortTableName))
 
 
-                  visitOccurrence <- dplyr::tbl(self$connection,
-                                            I(paste0(self$cdmSchema,
-                                                     ".visit_occurrence"))) |>
+                  self$visitOccurrence <- dplyr::tbl(self$connection,
+                                                     I(paste0(self$cdmSchema,
+                                                              ".visit_occurrence"))) |>
                     dplyr::select(person_id,
                                   visit_occurrence_id,
                                   visit_start_date,
                                   care_site_id) |>
                     dplyr::rename(subject_id = person_id) |>
-                    filter(care_site_id != 0)
+                    dplyr::filter(care_site_id != 0)
 
 
-                  careSite <- dplyr::tbl(self$connection,
-                                         I(paste0(self$cdmSchema,
-                                                  ".care_site"))) |>
+                  self$careSite <- dplyr::tbl(self$connection,
+                                              I(paste0(self$cdmSchema,
+                                                       ".care_site"))) |>
                     dplyr::select(care_site_id, x_institutioncode)
 
 
-                  dplyr::left_join(cohort,
-                                   visitOccurrence) |>
+                  rankCareSite <- private$rankingFunction()
+
+
+                  dplyr::left_join(self$cohort,
+                                   self$visitOccurrence) |>
                     dplyr::filter(between(visit_start_date,
-                                          sql("DATEADD(year, -1, cohort_start_date)"),
+                                          sql(sprintf("DATEADD(year, %s, cohort_start_date)",
+                                                      self$preDays*-1)),
                                           cohort_start_date)) |>
                     dplyr::distinct(cohort_definition_id,
                              subject_id,
                              visit_start_date,
                              .keep_all = TRUE) |>
-                    dplyr::left_join(careSite) |>
+                    dplyr::left_join(self$careSite) |>
                     dplyr::rename(station = x_institutioncode) |>
-                    dplyr::arrange(cohort_definition_id,
-                                   subject_id,
-                                   cohort_start_date,
-                                   desc(visit_start_date)) |>
+                    rankCareSite() |>
                     dplyr::distinct(cohort_definition_id,
                                     subject_id,
                                     cohort_start_date,
                                     .keep_all = TRUE) |>
-                    dplyr::select(-visit_occurrence_id,
-                                  -visit_start_date,
-                                  -care_site_id) |>
                     dplyr::collect() -> self$homeStation
 
                   invisible(self)
-
-                })
-
-
-HomeStation$set("public", "plot",
-                function(){
-
 
                 })
 
